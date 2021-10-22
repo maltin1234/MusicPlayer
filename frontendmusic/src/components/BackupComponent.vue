e86880bd4a9ef0ef869c8d2ae11cf57e50c0a60e
reddington12 <37213559+Reddington12@users.noreply.github.com>
2021-10-22 12:28:10 +0200

    interacting with yt api

--- frontendmusic/src/components/BackupComponent.vue
+++ frontendmusic/src/components/BackupComponent.vue
@@ -2,6 +2,21 @@
   <div class="mobile-screen">
     <b-container>
       <b-row>
+        <b-col cols="auto" class="mr-auto p-3">
+          <b-button><i class="bi bi-chevron-down"></i></b-button>
+        </b-col>
+
+        <b-col cols="12" class="p-5">
+          <b-img
+            thumbnail
+            fluid
+            src="https://picsum.photos/250/250/?image=54"
+            alt="Image 1"
+          ></b-img>
+        </b-col>
+        <b-col cols="12">
+          <p class="text"></p>
+        </b-col>
         <b-col>
           <ul v-if="info">
             <li v-for="(item, index) in info.items" :key="index.etag">
@@ -9,6 +24,26 @@
             </li>
           </ul>
         </b-col>
+      </b-row>
+      <b-row>
+        <div>
+          <label for="range-1">Example range with min and max</label>
+          <b-form-input
+            id="range-1"
+            v-model="value"
+            type="range"
+            min="0"
+            max="5"
+          ></b-form-input>
+        </div>
+      </b-row>
+      <b-row>
+        <b-card>
+          <b-col cols="2"><i class="bi bi-list-ul"></i></b-col>
+          <b-col cols="3"><i class="bi bi-skip-backward-fill"></i></b-col>
+          <b-col cols="3"><i class="bi bi-pause-circle"></i></b-col>
+          <b-col cols="3"><i class="bi bi-skip-forward-fill"></i></b-col>
+        </b-card>
       </b-row>
     </b-container>
   </div>
--- frontendmusic/src/main.js
+++ frontendmusic/src/main.js
@@ -1,27 +1,26 @@
-
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Vue from 'vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-
-
+import Vue from "vue";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-
-
+Vue.use(VueAxios, axios);
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(BootstrapVueIcons)
+Vue.use(BootstrapVueIcons);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+
   render: (h) => h(App),
 }).$mount("#app");
